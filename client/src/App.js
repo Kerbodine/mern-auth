a46f2d9f9b80a9e4023bda04c3fd4cc0f430e9cc
Michael Tong <michaelyuyangtong@gmail.com>
2022-01-31 08:37:16 +0800

    Added react-router-dom, signup and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" exact element={null} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
